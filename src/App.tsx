312cdfc73eed95b8a55b6500427786ad93470bf1
Víctor A. Javariz Vega <5726868-vjavariz@users.noreply.gitlab.com>
2020-07-06 22:03:17 -0500

    Implement state and change handler for the textarea input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import marked from "marked";
 
 const App = () => {
+  const [text, setText] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.currentTarget.value);
+  };
+
   return (
     <div>
       <h1>Markdown Previewer</h1>
+      <textarea id="editor" value={text} onChange={handleChange} />
+
+      <div id="preview"></div>
     </div>
   );
 };
