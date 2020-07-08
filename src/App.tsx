b10f29c3bf8a3236e92912b67321c7b297e82fc6
Víctor A. Javariz Vega <5726868-vjavariz@users.noreply.gitlab.com>
2020-07-08 00:20:37 -0500

    Style app with Ant Design and uninstall styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, ChangeEvent } from "react";
 import marked from "marked";
 import DOMPurify from "dompurify";
-import { Input } from "antd";
+import { Input, Typography, Row, Col, Layout, Card, Space } from "antd";
 
 import { defaultText } from "./data/defaultText";
 import "./App.css";
@@ -9,26 +9,51 @@ import "./App.css";
 const App: React.FC = () => {
   const [text, setText] = useState<string>(defaultText);
 
+  const { TextArea } = Input;
+  const { Title } = Typography;
+  const { Header, Content } = Layout;
+
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.currentTarget.value);
   };
 
-  marked.setOptions({ breaks: true });
+  marked.setOptions({ breaks: true }); // Add <br> on single line breaks (carriage returns)
 
   return (
-    <div className="App">
-      <h1>Markdown Previewer</h1>
-      <Input.TextArea
-        rows={8}
-        id="editor"
-        value={text}
-        onChange={handleChange}
-      />
+    <div>
+      <Layout>
+        <Typography>
+          <Header className="header">
+            <Title className="title">Markdown Previewer</Title>
+          </Header>
 
-      <div
-        id="preview"
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked(text)) }}
-      ></div>
+          <Content className="content">
+            <Row gutter={16}>
+              <Col span={12}>
+                <Card title="Input">
+                  <TextArea
+                    rows={20}
+                    id="editor"
+                    value={text}
+                    onChange={handleChange}
+                  />
+                </Card>
+              </Col>
+
+              <Col span={12}>
+                <Card title="Markdown">
+                  <div
+                    id="preview"
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(marked(text)),
+                    }}
+                  ></div>
+                </Card>
+              </Col>
+            </Row>
+          </Content>
+        </Typography>
+      </Layout>
     </div>
   );
 };
