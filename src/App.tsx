82f03f68c29444f4b607092305cf2c21eda15fd2
Víctor A. Javariz Vega <5726868-vjavariz@users.noreply.gitlab.com>
2020-07-07 21:21:25 -0500

    Replace insane with DOMPurify, add antd dependency, and set a default text value for the textarea field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, { useState, ChangeEvent } from "react";
 import marked from "marked";
+import DOMPurify from "dompurify";
+import { Input } from "antd";
 
-const App = () => {
-  const [text, setText] = useState<string>("");
+import { defaultText } from "./data/defaultText";
+import "./App.css";
+
+const App: React.FC = () => {
+  const [text, setText] = useState<string>(defaultText);
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.currentTarget.value);
@@ -11,13 +16,18 @@ const App = () => {
   marked.setOptions({ breaks: true });
 
   return (
-    <div>
+    <div className="App">
       <h1>Markdown Previewer</h1>
-      <textarea id="editor" value={text} onChange={handleChange} />
+      <Input.TextArea
+        rows={8}
+        id="editor"
+        value={text}
+        onChange={handleChange}
+      />
 
       <div
         id="preview"
-        dangerouslySetInnerHTML={{ __html: marked(text) }}
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked(text)) }}
       ></div>
     </div>
   );
